9aa3cb3c3f1bb82b8f969ecc5e25784b61bd700c
SebastianRdzC04 <22030138@uttcampus.edu.mx>
2024-11-10 23:42:39 -0600

    arreglando un errorsito que no me di cuenta, me falto la tabla de number en inventroy. arreglado

--- database/migrations/2024_10_18_161820_create_inventory_table.php
+++ database/migrations/2024_10_18_161820_create_inventory_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('inventory', function (Blueprint $table) {
             $table->id();
             $table->foreignId('serial_number_type_id')->constrained('serial_number_types_inventory')->onDelete('cascade');
+            $table->integer('number');
             $table->string('description', 100);
             $table->float('price', 5, 2);
             $table->enum('status', ['disponible', 'no disponible', 'en reparacion', 'en mantenimiento', 'dado de baja'])->default('disponible');
