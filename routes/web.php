0d64c3c75d46a34505cb9cefc865f90235ca849d
SebastianRdzC04 <22030138@uttcampus.edu.mx>
2024-11-19 12:07:52 -0600

    creo que ya estan unidas

--- routes/web.php
+++ routes/web.php
@@ -202,18 +202,6 @@ Route::post('dashboard/quote/event/{id}', function ($id, Request $request) {
 
 
 
-
-
-
-
-
-   return view('pages.dashboard');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
