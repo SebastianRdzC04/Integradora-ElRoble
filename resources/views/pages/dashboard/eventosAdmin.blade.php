d2ef4ab42060ab050bd6e9e9020ab80201325d02
SebastianRdzC04 <22030138@uttcampus.edu.mx>
2024-11-21 14:51:01 -0600

    solucionando error de compatibilidad de datos

--- resources/views/pages/dashboard/eventosAdmin.blade.php
+++ resources/views/pages/dashboard/eventosAdmin.blade.php
@@ -35,7 +35,9 @@
                     </div>
                     <div class="d-flex justify-content-between">
                         <div>
-                            <p>Lugar: {{ $event->quote->place ? $event->quote->place->name : $event->quote->package->place->name }} </p>
+                            <p>Lugar:
+                                {{ $event->quote->place ? $event->quote->place->name : $event->quote->package->place->name }}
+                            </p>
                         </div>
                         <!-- Calcula el tiempo que falta para que inicie el evento -->
                         <div>
@@ -100,22 +102,27 @@
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    @foreach ($event->quote->package->services as $service)
-                                                        <tr>
-                                                            <td> {{ $service->name }} </td>
-                                                            <td> {{ $service->pivot->description }} </td>
-                                                            <td> {{ $service->pivot->price }} </td>
-                                                            <td> {{ $service->pivot->cost }} </td>
-                                                        </tr>
-                                                    @endforeach
-                                                    @foreach ($event->quote->services as $service)
-                                                        <tr>
-                                                            <td> {{ $service->name }} </td>
-                                                            <td> {{ $service->description }} </td>
-                                                            <td> {{ $service->price }} </td>
-                                                            <td> {{ $service->cost }} </td>
-                                                        </tr>
-                                                    @endforeach
+                                                    @if ($event->quote->package)
+                                                        @foreach ($event->quote->package->services as $service)
+                                                            <tr>
+                                                                <td> {{ $service->name }} </td>
+                                                                <td> {{ $service->pivot->description }} </td>
+                                                                <td> {{ $service->pivot->price }} </td>
+                                                                <td> {{ $service->pivot->cost }} </td>
+                                                            </tr>
+                                                        @endforeach
+                                                    @endif
+                                                    @if ($event->services)
+                                                        @foreach ($event->quote->services as $service)
+                                                            <tr>
+                                                                <td> {{ $service->name }} </td>
+                                                                <td> {{ $service->description }} </td>
+                                                                <td> {{ $service->price }} </td>
+                                                                <td> {{ $service->cost }} </td>
+                                                            </tr>
+                                                        @endforeach
+
+                                                    @endif
                                                 </tbody>
                                             </table>
                                         </div>
