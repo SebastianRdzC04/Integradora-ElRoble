cf2246abf5594a3e6ba469405196edb58e7c7f52
XxjesusalbertoxX <villarrealperezjesusalberto@gmail.com>
2024-11-24 22:14:08 -0600

    Comit con el login terminado solo falta facebook
    se validaron campos y mejoraron las vistas.
    
    se cambio el calendario al poner el birthdate por 3 selects
    
    se creo el trigger de la edad, para la db y ya valida solo mayores de edad para la cuenta

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $fillable = [
         'nickname',
         'email',
+        'email_verified_at',
         'password',
         'person_id',
         'avatar',
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
             $table->string('external_auth')->nullable();
             $table->timestamps();
         });
+
     }
 
     /**
