f3688490a57f09d2374a6c9d21a7674ece828aad
SebastianRdzC04 <22030138@uttcampus.edu.mx>
2024-12-01 22:40:42 -0600

    estandarizando las tablas al mimso tamaîo todos

--- resources/views/pages/dashboard/packages.blade.php
+++ resources/views/pages/dashboard/packages.blade.php
@@ -11,7 +11,7 @@
 
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-10">
+            <div class="col-12">
                 <div class="table-responsive pack-table">
                     <table class="table shadow" id="tabla-paquetes">
                         <thead>
