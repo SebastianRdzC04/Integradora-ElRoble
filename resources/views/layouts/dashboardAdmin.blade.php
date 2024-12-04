6b9458e1c0c2f2fb64d9a7d0bd3765f12f2c8e3f
XxjesusalbertoxX <villarrealperezjesusalberto@gmail.com>
2024-12-04 14:07:51 -0600

    incidentes con barra lateral incluida

--- resources/views/layouts/dashboardAdmin.blade.php
+++ resources/views/layouts/dashboardAdmin.blade.php
@@ -55,6 +55,27 @@
     <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/core@6.1.15/index.global.min.js'></script>
     <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/web-component@6.1.15/index.global.min.js'></script>
     <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@6.1.15/index.global.min.js'></script>
+    
+    <script>
+            toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": true,
+        "positionClass": "toast-top-right", 
+        "preventDuplicates": true,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000", 
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "slideDown",
+        "hideMethod": "fadeOut"
+    };
+
+    </script>
     @yield('styles')
 
     <style>
--- resources/views/pages/inventory/report_incidents.blade.php
+++ resources/views/pages/inventory/report_incidents.blade.php
@@ -1,17 +1,13 @@
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Reportar Incidencia</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-
+@extends('layouts.dashboardAdmin')
+    @section('meta')
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    @endsection
+    
+    @section('title')
+        Reportar Incidencia
+    @endsection
+   
+    @section('styles')
     <style>
         body {
             min-height: 100dvh;
@@ -28,28 +24,7 @@
             }
         }
     </style>
-    <script>
-    toastr.options = {
-        "closeButton": true,
-        "debug": false,
-        "newestOnTop": true,
-        "progressBar": true,
-        "positionClass": "toast-top-right", // Cambia la posición si lo necesitas
-        "preventDuplicates": true,
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "5000", // Duración de la notificación
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "slideDown",
-        "hideMethod": "fadeOut"
-    };
-    </script>
-
-</head>
-<body>
+    @endsection
 
 
 <!-- Mensaje de éxito -->
@@ -82,7 +57,7 @@
 
 <!-- formulario incidente globlal -->
  
-    
+@section('content')
 
 <div class="container mt-4">
     <h2>Reportar Incidencia</h2>
@@ -190,8 +165,9 @@
             </div>
         </div>
     </div>        
-    
+@endsection
 
+@section('scripts')
     <script>
     const fileInput = document.getElementById('images');
 
@@ -444,8 +420,6 @@ function populateCategories(categories) {
 
 </script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+@endsection
 </body>
 </html>
