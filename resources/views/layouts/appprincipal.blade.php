35814034996e4ddf82aa7bad56043d7c3453e6df
XxjesusalbertoxX <villarrealperezjesusalberto@gmail.com>
2024-12-07 01:24:18 -0600

    navbar cambiado el antiguo tenia muchos problemas, se rediseño para mayor eficiencia y las vistas, tambien el app quedo limpio lo pesado se esta importando desde otros blade.
    IMPORTANTEÑ en el blade del navbar se esta importando el app.css por si no se encuentra es por que esta ahi.
    
    app.css esta administrando el diseño cualquier cambio es desde ahi o directamente en el atributo style
    
    Esta es la buena.

--- resources/views/layouts/appprincipal.blade.php
+++ resources/views/layouts/appprincipal.blade.php
@@ -5,248 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
     <title>El Roble</title>
-    <style>
-
-    </style>
+    <!--el app esta en el blade del navbar para optimizar espacio hay esta el script del navbar-->
 </head>
 <body>
+<!--importando el blade del navbar-->
     @include('layouts.navbar')
 
     <main style="min-height: 200vh;">
-    <div class="custom-bg custom-back-image" style="background-image: url('{{ asset('js/images/El Roble principal image.jpg') }}');">
-        <!--dentro de aqui debe de ir el texto-->
+    <div class="custom-bg custom-back-image align-content-center" style="background-image: url('{{ asset('js/images/El Roble principal image.jpg') }}');">
+        
+        <div class="d-flex justify-content-center text-bg-info">
+            <div>
+                <p class="text-image-front p-3">Este es el texto de muestra</p>
+            </div>
+        </div>
     </div>
     <!--dentro de aqui debe de ir todo el contenido de la pagina-->
-    <section id="tranding">
-      <div class="container">
-        <h3 class="text-center section-subheading">- popular Delivery -</h3>
-        <h1 class="text-center section-heading">Tranding food</h1>
-      </div>
-      <div class="container">
-        <div class="swiper tranding-slider">
-          <div class="swiper-wrapper">
-            <!-- Slide-start -->
-            <div class="swiper-slide tranding-slide">
-              <div class="tranding-slide-img">
-                <img src="{{asset('images/imagen1.jpg')}}" alt="Tranding">
-              </div>
-              <div class="tranding-slide-content">
-                <h1 class="food-price">$20</h1>
-                <div class="tranding-slide-content-bottom">
-                  <h2 class="food-name">
-                    Special Pizza
-                  </h2>
-                  <h3 class="food-rating">
-                    <span>4.5</span>
-                    <div class="rating">
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                    </div>
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <!-- Slide-end -->
-            <!-- Slide-start -->
-            <div class="swiper-slide tranding-slide">
-              <div class="tranding-slide-img">
-                <img src="{{asset('images/imagen8.jpg')}}" alt="Tranding">
-              </div>
-              <div class="tranding-slide-content">
-                <h1 class="food-price">$20</h1>
-                <div class="tranding-slide-content-bottom">
-                  <h2 class="food-name">
-                    Meat Ball
-                  </h2>
-                  <h3 class="food-rating">
-                    <span>4.5</span>
-                    <div class="rating">
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                    </div>
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <!-- Slide-end -->
-            <!-- Slide-start -->
-            <div class="swiper-slide tranding-slide">
-              <div class="tranding-slide-img">
-                <img src="{{asset('images/imagen7.jpg')}}" alt="Tranding">
-              </div>
-              <div class="tranding-slide-content">
-                <h1 class="food-price">$40</h1>
-                <div class="tranding-slide-content-bottom">
-                  <h2 class="food-name">
-                    Burger
-                  </h2>
-                  <h3 class="food-rating">
-                    <span>4.5</span>
-                    <div class="rating">
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                    </div>
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <!-- Slide-end -->
-            <!-- Slide-start -->
-            <div class="swiper-slide tranding-slide">
-              <div class="tranding-slide-img">
-                <img src="{{asset('images/imagen6.jpg')}}" alt="Tranding">
-              </div>
-              <div class="tranding-slide-content">
-                <h1 class="food-price">$15</h1>
-                <div class="tranding-slide-content-bottom">
-                  <h2 class="food-name">
-                    Frish Curry
-                  </h2>
-                  <h3 class="food-rating">
-                    <span>4.5</span>
-                    <div class="rating">
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                    </div>
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <!-- Slide-end -->
-            <!-- Slide-start -->
-            <div class="swiper-slide tranding-slide">
-              <div class="tranding-slide-img">
-                <img src="{{asset('images/imagen5.jpg')}}" alt="Tranding">
-              </div>
-              <div class="tranding-slide-content">
-                <h1 class="food-price">$15</h1>
-                <div class="tranding-slide-content-bottom">
-                  <h2 class="food-name">
-                    Pane Cake
-                  </h2>
-                  <h3 class="food-rating">
-                    <span>4.5</span>
-                    <div class="rating">
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                    </div>
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <!-- Slide-end -->
-            <!-- Slide-start -->
-            <div class="swiper-slide tranding-slide">
-              <div class="tranding-slide-img">
-                <img src="{{asset('images/imagen4.jpg')}}" alt="Tranding">
-              </div>
-              <div class="tranding-slide-content">
-                <h1 class="food-price">$20</h1>
-                <div class="tranding-slide-content-bottom">
-                  <h2 class="food-name">
-                    Vanilla cake
-                  </h2>
-                  <h3 class="food-rating">
-                    <span>4.5</span>
-                    <div class="rating">
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                    </div>
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <!-- Slide-end -->
-            <!-- Slide-start -->
-            <div class="swiper-slide tranding-slide">
-              <div class="tranding-slide-img">
-                <img src="{{asset('images/imagen3.jpg')}}" alt="Tranding">
-              </div>
-              <div class="tranding-slide-content">
-                <h1 class="food-price">$8</h1>
-                <div class="tranding-slide-content-bottom">
-                  <h2 class="food-name">
-                    Straw Cake
-                  </h2>
-                  <h3 class="food-rating">
-                    <span>4.5</span>
-                    <div class="rating">
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                    </div>
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <!-- Slide-end -->
-          </div>
 
-          <div class="tranding-slider-control">
-            <div class="swiper-button-prev slider-arrow">
-              <ion-icon name="arrow-back-outline"></ion-icon>
-            </div>
-            <div class="swiper-button-next slider-arrow">
-              <ion-icon name="arrow-forward-outline"></ion-icon>
-            </div>
-            <div class="swiper-pagination"></div>
-          </div>
+<!--importando el blade del carousel-->
+    @include('layouts.carousel.carousel')
 
-        </div>
-      </div>
-    </section>
-    
-    
-</div>
+  </main>
 
-</main>
-
-
-<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-<script> var TrandingSlider = new Swiper('.tranding-slider', {
-  effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  loop: true,
-  slidesPerView: 'auto',
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 2.5,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-});</script>
 </body>
 </html>
+i
