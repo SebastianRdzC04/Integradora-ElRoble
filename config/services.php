d3592057d06d65e1f10e4c65f729174d5f2c68f8
XxjesusalbertoxX <villarrealperezjesusalberto@gmail.com>
2024-12-09 22:56:36 -0600

    listo para mesclar cotizaciones testeando incio de sesion

--- config/services.php
+++ config/services.php
@@ -49,4 +49,10 @@ return [
     'redirect' => env('FACEBOOK_REDIRECT_URI'),
     ],
 
+    'cloudinary' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key' => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
+
 ];
